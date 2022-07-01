ed0675afdef409916f5563f1825d78ba64feb931
Renan Andrade <Fel9.renan02@gmail.com>
2022-07-01 18:55:35 -0300

    Added edit article functionality

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -10,13 +10,15 @@ export default function Articles() {
   const [isVisible, setIsVisible] = useState(false);
   // const [loading, setLoading] = useState(true);
   const [articles, setArticles] = useState([]);
+  const [article, setArticle] = useState({});
   const [response, setResponse] = useState({});
   const [show, setShow] = useState(false);
+  const [editing, setEditing] = useState(false);
 
   // fetch all data when response changes
   useEffect(() => {
     async function fetchArticles() {
-      await fetch("http://localhost:3000/articles").then((response) =>
+      await fetch("http://192.168.0.101:3000/articles").then((response) =>
         response.json().then((data) => setArticles(data))
       );
     }
@@ -26,7 +28,7 @@ export default function Articles() {
 
   // show notification when response changes
   useEffect(() => {
-    if (response.error || response.error === false) setShow(true);
+    if (response.error) setShow(true);
     setIsVisible(false);
     setTimeout(() => setShow(false), 3000);
   }, [response]);
@@ -36,30 +38,47 @@ export default function Articles() {
       <Navbar />
       <div className="container">
         <h2 className="title mt-4 px-4">Articles</h2>
-        {articles.map((article, index) => (
+        {editing ? (
           <Card
-            title={article.title}
-            key={index}
             article={article}
+            setEditing={setEditing}
             setResponse={setResponse}
+            editing
             deleteIcon
-          >
-            <p className="card-content">{article.text}</p>
-          </Card>
-        ))}
+          />
+        ) : (
+          articles.map((article, index) => (
+            <Card
+              title={article.title}
+              key={index}
+              article={article}
+              setArticle={setArticle}
+              setResponse={setResponse}
+              setEditing={setEditing}
+              deleteIcon
+              editIcon
+            >
+              <p className="content" style={{ wordBreak: "break-all" }}>
+                {article.text}
+              </p>
+            </Card>
+          ))
+        )}
       </div>
 
-      <button
-        onClick={() => setIsVisible(true)}
-        style={{ position: "fixed", right: 20, bottom: 20 }}
-        className={"button is-primary"}
-      >
-        Create
-      </button>
+      {!editing && (
+        <button
+          onClick={() => setIsVisible(true)}
+          style={{ position: "fixed", right: 20, bottom: 20 }}
+          className={"button is-link"}
+        >
+          Create
+        </button>
+      )}
 
       {response.error ? (
         <Notification show={show} variant={"is-danger"}>
-          An error has occured
+          An error has occured: {response.message}
         </Notification>
       ) : (
         <Notification show={show} variant={"is-success"}>
@@ -68,7 +87,7 @@ export default function Articles() {
       )}
 
       <Modal isVisible={isVisible} setIsVisible={setIsVisible}>
-        <ArticleForm setResponse={setResponse} />
+        <ArticleForm setResponse={setResponse} setIsVisible={setIsVisible} />
       </Modal>
     </>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,41 +1,161 @@
+import { useState } from "react";
 import trashIcon from "../svg/delete-white.svg";
+import editIcon from "../svg/white-pencil-edit.svg";
 
-export default function Card({ title, children, style, article, ...props }) {
+export default function Card({
+  title,
+  children,
+  style,
+  article,
+  setArticle,
+  setEditing,
+  setResponse,
+  ...props
+}) {
+  const [formState, setFormState] = useState(article);
+
+  const editArticle = (article) => {
+    setArticle(article);
+    setEditing(true);
+  };
+
   const deleteArticle = async (articleId) => {
-    const rawResponse = await fetch(
-      `http://localhost:3000/articles/${articleId}`,
-      {
-        method: "DELETE",
-      }
-    ).then((response) => response.json().then((data) => setArticles(data)));
-    const response = await rawResponse.text();
-    console.log(response);
+    await fetch(`http://localhost:3000/articles/${articleId}`, {
+      method: "DELETE",
+    })
+      .then(async (response) =>
+        setResponse({
+          message: await response.text(),
+          error: false,
+          status: response.status,
+        })
+      )
+      .catch((error) =>
+        setResponse({
+          message: error.message,
+          error: true,
+          status: error,
+        })
+      );
+  };
+
+  const updateArticle = async () => {
+    await fetch(`http://192.168.0.101:3000/articles/${article._id}`, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "PATCH",
+      body: JSON.stringify(formState),
+    })
+      .then(async (response) => {
+        setResponse({
+          message: await response.text(),
+          error: false,
+          status: response.status,
+        });
+        setEditing(false);
+      })
+      .catch((error) => {
+        setResponse({
+          message: error.message,
+          error: true,
+          status: error,
+        });
+        setEditing(false);
+      });
+  };
+
+  const handleChange = (event) => {
+    setFormState({ ...formState, [event.target.name]: event.target.value });
   };
 
   return (
     <div className="card p-4">
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "start",
-          justifyContent: "space-between",
-          ...style,
-        }}
-      >
-        <h2 className="title is-4">{title}</h2>
-        {props.deleteIcon && (
-          <button
-            className="button is-danger"
-            onClick={() => {
-              deleteArticle(article._id);
+      {props.editing ? (
+        <>
+          <div className="mb-4" style={{ ...style }}>
+            {/* if editing  */}
+            <input
+              className="title is-4 input"
+              name="title"
+              defaultValue={article.title}
+              onChange={handleChange}
+            />
+          </div>
+          <section className="control">
+            <textarea
+              className="textarea"
+              name="text"
+              placeholder="Digite o artigo"
+              defaultValue={article.text}
+              onChange={(event) => handleChange(event)}
+            />
+          </section>
+          <span className="buttons">
+            <button
+              onClick={updateArticle}
+              className="button is-primary is-fullwidth mt-2"
+            >
+              Update
+            </button>
+            <button
+              className="button is-light is-fullwidth"
+              onClick={() => setEditing(false)}
+            >
+              Cancel
+            </button>
+          </span>
+        </>
+      ) : (
+        <>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "start",
+              justifyContent: "space-between",
+              ...style,
             }}
           >
-            <img src={trashIcon} alt="delete icon" width={"25px"} />
-          </button>
-        )}
-      </div>
-      {children}
+            {/* if not editing  */}
+            <h2 className="title is-4 mb-4">{title}</h2>
+            <span className="buttons are-small" style={{ flexWrap: "nowrap" }}>
+              {props.deleteIcon && (
+                <button
+                  className="button is-danger"
+                  onClick={() => {
+                    deleteArticle(article._id);
+                  }}
+                >
+                  <img
+                    src={trashIcon}
+                    alt="delete icon"
+                    width={"20px"}
+                    style={{ minWidth: "20px" }}
+                  />
+                </button>
+              )}
+              {props.editIcon && (
+                <button
+                  className="button is-primary"
+                  onClick={() => {
+                    editArticle(article);
+                  }}
+                >
+                  <img
+                    src={editIcon}
+                    alt="edit icon"
+                    width={"20px"}
+                    style={{ minWidth: "20px" }}
+                  />
+                </button>
+              )}
+            </span>
+          </div>
+          {children}
+        </>
+      )}
     </div>
   );
 }
