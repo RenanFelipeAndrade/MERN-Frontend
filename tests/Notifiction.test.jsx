5eb7f36c13cb967afb542cf17bf312bd6e184ef3
Renan Andrade <Fel9.renan02@gmail.com>
2022-08-06 13:01:53 +0000

    Added notification test

---
+++ tests/Notifiction.test.jsx
@@ -0,0 +1,31 @@
+import { cleanup, render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { afterEach } from "vitest";
+import { Notification } from "../src/components/Notification";
+
+afterEach(() => {
+  cleanup();
+});
+
+describe("A notification test", () => {
+  it("Renders the notification component", () => {
+    const component = render(
+      <Notification show={true} variant={"is-primary"}>
+        A notification!
+      </Notification>
+    );
+    expect(component).toBeInTheDocument();
+  });
+
+  it("Checks if child container is a span", () => {
+    render(
+      <Notification show={true} variant={"is-primary"}>
+        A notification!
+      </Notification>
+    );
+
+    const component = screen.getByText("A notification!");
+
+    expect(component.nodeName).toBe("SPAN");
+  });
+});
