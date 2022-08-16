e5a4a51be095deacbcd926cec34b72220d1e606a
Renan Andrade <Fel9.renan02@gmail.com>
2022-08-16 23:53:49 +0000

    Corrected card title breaking issue

--- src/components/Card/ListItem.jsx
+++ src/components/Card/ListItem.jsx
@@ -41,53 +41,61 @@ export const ListItem = ({
   return (
     <>
       <div
-        className={`is-flex is-flex-direction-row is-justify-content-space-between is-align-items-center ${props.className}`}
+        className={`is-flex is-flex-direction-row is-justify-content-space-between`}
       >
         <div className="mb-4">
-          <h2 className="title is-3">{title || article.title}</h2>
-          <div className="is-flex is-flex-direction-column is-justify-content-space-between">
-            <span className="mb-2">
-              Created at: {formatDate(article.createdAt)}
-            </span>
-            <h3 className="subtitle is-6">
-              Author: {formatAuthorName(article.author)}
-            </h3>
-          </div>
+          {article ? (
+            <>
+              <h2 className="title is-3">{article.title}</h2>
+              <div className="is-flex is-flex-direction-column is-justify-content-space-between">
+                <span className="mb-2">
+                  Created at: {formatDate(article.createdAt)}
+                </span>
+                <h3 className="subtitle is-6">
+                  Author: {formatAuthorName(article.author)}
+                </h3>
+              </div>
+            </>
+          ) : (
+            <h2 className="title is-3">{title}</h2>
+          )}
         </div>
 
-        {/* edit and delete icons, if those exist */}
-        <span className="buttons are-small is-justify-content-flex-end">
-          {props.deleteIcon && (
-            <button
-              className="button is-danger"
-              onClick={() => {
-                deleteArticle(article._id);
-              }}
-            >
-              <img
-                src={trashIcon}
-                alt="delete icon"
-                width={"20px"}
-                style={{ minWidth: "20px" }}
-              />
-            </button>
-          )}
-          {props.editIcon && (
-            <button
-              className="button is-primary mr-2"
-              onClick={() => {
-                editArticle(article);
-              }}
-            >
-              <img
-                src={editIcon}
-                alt="edit icon"
-                width={"20px"}
-                style={{ minWidth: "20px" }}
-              />
-            </button>
-          )}
-        </span>
+        {/* edit or delete icons, if those exist */}
+        {props.deleteIcon || props.editIcon ? (
+          <span className="buttons are-small is-justify-content-flex-end">
+            {props.deleteIcon && (
+              <button
+                className="button is-danger"
+                onClick={() => {
+                  deleteArticle(article._id);
+                }}
+              >
+                <img
+                  src={trashIcon}
+                  alt="delete icon"
+                  width={"20px"}
+                  style={{ minWidth: "20px" }}
+                />
+              </button>
+            )}
+            {props.editIcon && (
+              <button
+                className="button is-primary mr-2"
+                onClick={() => {
+                  editArticle(article);
+                }}
+              >
+                <img
+                  src={editIcon}
+                  alt="edit icon"
+                  width={"20px"}
+                  style={{ minWidth: "20px" }}
+                />
+              </button>
+            )}
+          </span>
+        ) : null}
       </div>
       {children}
     </>
